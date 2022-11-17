e2043dc914edd1165c0309bcc7bd319c6ff082b1
Listandsell2021 <84175604+Listandsell2021@users.noreply.github.com>
2022-11-17 03:10:53 +0100

    Update index.js

--- src/pages/pages/pricing/index.js
+++ src/pages/pages/pricing/index.js
@@ -218,7 +218,7 @@ const handleChangePerMonthPriceUYearly= e => {
   )
 }
 
-export const getserversideprops = async () => {
+export const getServerSideProps = async () => {
   
   const res1 = await axios.post('https://umzungcrmtest.vercel.app/api/getPackages')
   //const apiData = res.data

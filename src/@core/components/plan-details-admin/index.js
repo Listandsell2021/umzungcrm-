33bb683c199a2de3d4c9b902921a1115af1078ae
Listandsell2021 <84175604+Listandsell2021@users.noreply.github.com>
2022-11-18 02:49:21 +0100

    Update index.js

--- src/@core/components/plan-details-admin/index.js
+++ src/@core/components/plan-details-admin/index.js
@@ -245,74 +245,27 @@ const BpCheckedIcon = styled(BpIcon)({
   }
 function setdata()
 {
-  /*var datanew={
-    "pricingPlans":
-    [{"imgWidth":{"$numberInt":"100"},
-    "title":"BasicTest",
-    "imgHeight":{"$numberInt":"100"},
-    "monthlyPrice":{"$numberInt":"0"},
-    "currentPlan":true,
-    "popularPlan":false,
-    "subtitle":"A simple start for everyone",
-    "imgSrc":"/images/pages/pricing-illustration-1.png",
-    "yearlyPlan":
-    {"perMonth":{"$numberInt":"0"},
-    "totalAnnual":{"$numberInt":"0"}},
-    "planBenefits":
-    ["100 responses a month",
-    "Unlimited forms and surveys",
-    "Unlimited fields","Basic form creation tools",
-    "Up to 2 subdomains"]},
-    {"imgWidth":{"$numberInt":"100"},
-    "title":"BasicTest",
-    "imgHeight":{"$numberInt":"100"},
-    "monthlyPrice":{"$numberInt":"0"},
-    "currentPlan":true,
-    "popularPlan":false,
-    "subtitle":"A simple start for everyone",
-    "imgSrc":"/images/pages/pricing-illustration-1.png",
-    "yearlyPlan":{"perMonth":{"$numberInt":"0"},
-    "totalAnnual":{"$numberInt":"0"}},
-    "planBenefits":["100 responses a month",
-    "Unlimited forms and surveys",
-    "Unlimited fields",
-    "Basic form creation tools",
-    "Up to 2 subdomains"]},
-    {"imgWidth":{"$numberInt":"100"},
-    "imgHeight":{"$numberInt":"100"},
-    "monthlyPrice":{"$numberInt":"49"},
-    "title":"Standard",
-    "popularPlan":true,
-    "currentPlan":false,
-    "subtitle":"For small to medium businesses",
-    "imgSrc":"/images/pages/pricing-illustration-2.png",
-    "yearlyPlan":{"perMonth":{"$numberInt":"40"},
-    "totalAnnual":{"$numberInt":"480"}},
-    "planBenefits":["Unlimited responses","Unlimited forms and surveys",
-    "Instagram profile page","Google Docs integration","Custom “Thank you” page"]},
-    {"imgWidth":{"$numberInt":"100"},"imgHeight":{"$numberInt":"100"},
-    "monthlyPrice":{"$numberInt":"99"},
-    "popularPlan":false,
-    "currentPlan":false,
-    "title":"Enterprise",
-    "subtitle":"Solution for big organizations",
-    "imgSrc":"/images/pages/pricing-illustration-3.png",
-    "yearlyPlan":{"perMonth":{"$numberInt":"80"},
-    "totalAnnual":{"$numberInt":"960"}},
-    "planBenefits":["PayPal payments",
-    "Logic Jumps","File upload with 5GB storage","Custom domain support","Stripe integration"]}],
-    
-    
-    
-    "faq":
-    [{"id":"general-settings",
-    "question":"General settings",
-    "answer":"Sesame snaps tart bonbon tiramisu jelly beans lemon drops bear claw candy gummi bears. Caramels pudding sweet donut tootsie roll gummies macaroon. Lemon drops caramels sesame snaps dessert jujubes. Cupcake chocolate bonbon cake tiramisu. Gummies candy canes ice cream biscuit. Jelly gummies wafer danish chupa chups sugar plum cookie."},
-    {"id":"users","question":"Users","answer":"Chocolate sweet roll lemon drops chocolate cake candy canes halvah. Donut fruitcake sweet roll brownie carrot cake cake. Donut jujubes pudding candy macaroon. Gummies gingerbread croissant bonbon. Cookie toffee cupcake cotton candy candy canes dessert cotton candy liquorice. Jelly beans gummi bears toffee chocolate bar chocolate cake."},
-    {"id":"advanced-settings","question":"Advanced settings","answer":"Halvah liquorice pastry marshmallow sugar plum. Dessert chocolate pastry gummi bears pastry. Gingerbread bonbon pudding oat cake jujubes pie wafer tart brownie. Soufflé jujubes icing powder liquorice. Sweet donut toffee liquorice dessert dragée. Topping cake danish chupa chups chupa chups gummies. Cotton candy gummies chocolate cake oat cake."}]}
-  console.log(datanew)*/
   
+var storedData = window.localStorage.getItem('userData')
+      storedData=JSON.parse(storedData)
+      
+  var data={
+        "sa_id":"sa1",
+        "a_id":storedData.id,
+        "address":address,
+        "token":"123333",
+        "date_registered":"211515",
+        "avatar":"/images/avatars/4.png",
+        "contact":phone,
+        "currentPlan":data?.title,
+        "full_name":storedData.fullName,
+        "role":storedData.role,
+        "status":"pending",
+        "email":email,
+        "username":email}
+
   
+  console.log(data)
 
 }
   return (

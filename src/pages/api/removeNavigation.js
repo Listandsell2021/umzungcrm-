14e473331152a3565c8c2ccf62e3fa9d4b13f6b2
Listandsell2021 <84175604+Listandsell2021@users.noreply.github.com>
2022-11-23 00:51:26 +0100

    Update removeNavigation.js

--- src/pages/api/removeNavigation.js
+++ src/pages/api/removeNavigation.js
@@ -3,10 +3,10 @@ import { connectToDatabase } from "./mongodbnew";
 export default async function handler(req, res) {
   const { db } = await connectToDatabase();
 
-     const{navigationData}=req.body;
+     const{deletenavigation}=req.body;
 
 
-  const todos = await db.collection("Navigation_crm").deleteOne({"global_id" : navigationData.global_id,title:navigationData.tittle});
+  const todos = await db.collection("Navigation_crm").deleteOne({"global_id" : deletenavigation.global_id,title:deletenavigation.tittle});
   
   res.status(200).json(todos);
   //await db.close();

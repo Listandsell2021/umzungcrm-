2ccc561ec14578c6e402c4d263a50d42fba35773
Listandsell2021 <84175604+Listandsell2021@users.noreply.github.com>
2022-11-29 13:18:31 +0100

    s

--- src/views/apps/user/list/TableHeader.js
+++ src/views/apps/user/list/TableHeader.js
@@ -12,9 +12,9 @@ const TableHeader = props => {
 
   return (
     <Box sx={{ p: 5, pb: 3, display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-between' }}>
-      <Button sx={{ mr: 4, mb: 2 }} color='secondary' variant='outlined' startIcon={<ExportVariant fontSize='small' />}>
+      {/* <Button sx={{ mr: 4, mb: 2 }} color='secondary' variant='outlined' startIcon={<ExportVariant fontSize='small' />}>
         Export
-      </Button>
+      </Button> */}
       <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center' }}>
         <TextField
           size='small'
